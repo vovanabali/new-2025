7537c4693572f075054c59451af0a2b6235d8cdb
Slava <vovanabali@mail.ru>
2024-12-10 18:46:37 +0300

    Delete phaserjs_editor_scripts_base directory

--- phaserjs_editor_scripts_base/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ScriptNode } from "./ScriptNode.js";
-export { default as UserComponent } from "./UserComponent.js";
-export { default as ActionTargetComp } from "./ActionTargetComp.js";
